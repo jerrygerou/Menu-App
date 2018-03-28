9695c9cba2e5810d255a52184b827ece90b6f7c5
Jerry Gerou <jerrysgerou@gmail.com>
2018-03-28 07:39:52 -0500

    Order total working

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -5,12 +5,4 @@ class OrderItem < ApplicationRecord
     menu_item.price * quantity
   end
 
-  def order_total
-    total = 0
-    order_item = OrderItem.where(in_order: true)
-    order_item.each do |item|
-      total += (item.menu_item.price * item.quantity)
-    end
-  end
-
 end
--- db/migrate/20180327154018_create_orders.rb
+++
@@ -1,9 +0,0 @@
-class CreateOrders < ActiveRecord::Migration[5.1]
-  def change
-    create_table :orders do |t|
-      t.decimal :total, precision: 8, scale: 2
-
-      t.timestamps
-    end
-  end
-end
